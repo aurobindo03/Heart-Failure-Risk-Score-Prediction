5a0807c2ca1ce8bdb47ca13898886464b952259a
Ishita <ishitasrivastava013@gmail.com>
2026-04-19 22:05:34 +0530

    demo site

--- Demo/backend/dice_api.py
+++ Demo/backend/dice_api.py
@@ -329,7 +329,7 @@
 # if __name__ == "__main__":
 #     app.run(host="0.0.0.0", port=8000, debug=True)
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS # type: ignore
 import pandas as pd
 import joblib
 import dice_ml
